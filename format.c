6f546fffc69c751830f713008a3f90c73787ed5b
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-04-22 23:13:05 +0100

    task 1

--- format.c
+++ format.c
@@ -47,3 +47,29 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (printed_chars);
 }
+/**
+ * print_integer - prints an integer
+ * @n: integer to print
+ *
+ * Return: number of digits printed
+ */
+int print_integer(int n)
+{
+	int count = 0;
+
+	if (n == 0)
+		return (putchar('0'));
+
+	if (n < 0)
+	{
+		count += putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		count += print_integer(n / 10);
+
+	count += putchar(n % 10 + '0');
+
+	return (count);
+}
