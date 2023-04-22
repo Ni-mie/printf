faf8c74905b05f1280107ac8444b9828d5017a04
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-04-22 23:16:36 +0100

    task 1

--- format.c
+++ format.c
@@ -30,10 +30,6 @@ int _printf(const char *format, ...)
 		case 's':
 			printed_chars += printf("%s", va_arg(args, char *));
 			break;
-		case 'd':
-		case 'i':
-			printed_chars += print_integer(va_arg(args, int));
-			break;
 		case '%':
 			putchar('%');
 			printed_chars++;
@@ -46,30 +42,4 @@ int _printf(const char *format, ...)
 	}
 	va_end(args);
 	return (printed_chars);
-}
-/**
- * print_integer - prints an integer
- * @n: integer to print
- *
- * Return: number of digits printed
- */
-int print_integer(int n)
-{
-	int count = 0;
-
-	if (n == 0)
-		return (putchar('0'));
-
-	if (n < 0)
-	{
-		count += putchar('-');
-		n = -n;
-	}
-
-	if (n / 10)
-		count += print_integer(n / 10);
-
-	count += putchar(n % 10 + '0');
-
-	return (count);
 }
