89a12dd12949036dde8bd95ac14531897b6e99ba
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-04-22 23:28:36 +0100

    figuring it out

--- format.c
+++ format.c
@@ -7,69 +7,43 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int printed = 0;
-	char *str;
+	int printed_chars = 0;
+	char c;
 
 	va_start(args, format);
 
-	while (*format)
+	while ((c = *format++) != '\0')
 	{
-		if (*format == '%')
+		if (c != '%')
 		{
-			format++;
-			switch (*format)
-			{
-			case 'c':
-				printed += putchar(va_arg(args, int));
-				break;
-			case 's':
-				printed += printf("%s", va_arg(args, char *));
-				break;
-			case 'd':
-			case 'i':
-				printed += print_integer(va_arg(args, int));
-				break;
-			case '%':
-				printed += putchar('%');
-				break;
-			default:
-				printed += putchar('%');
-				printed += putchar(*format);
-				break;
-			}
+			putchar(c);
+			printed_chars++;
+			continue;
 		}
-		else
+
+		switch (*format++)
 		{
-			printed += putchar(*format);
+		case 'c':
+			putchar(va_arg(args, int));
+			printed_chars++;
+			break;
+
+		case 's':
+			printed_chars += printf("%s", va_arg(args, char *));
+			break;
+
+		case '%':
+			putchar('%');
+			printed_chars++;
+			break;
+
+		default:
+			putchar('%');
+			putchar(c);
+			printed_chars += 2;
 		}
-		format++;
 	}
+
 	va_end(args);
-	return (printed);
+	return (printed_chars);
 }
-/**
- * print_integer - prints an integer
- * @n: integer to print
- *
- * Return: number of digits printed
- */
-int print_integer(int n)
-{
-	int count = 0;
-
-	if (n == 0)
-		return (putchar('0'));
-
-	if (n < 0)
-	{
-		count += putchar('-');
-		n = -n;
-	}
-
-	if (n / 10)
-		count += print_integer(n / 10);
-
-	count += putchar(n % 10 + '0');
-
-	return (count);
-}}
