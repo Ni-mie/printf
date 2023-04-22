66730445eb3808034742af43818b1427c9973bf5
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-04-22 23:23:18 +0100

    figuring it out

--- format.c
+++ format.c
@@ -58,18 +58,18 @@ int print_integer(int n)
 	int count = 0;
 
 	if (n == 0)
-		return (_putchar('0'));
+		return (putchar('0'));
 
 	if (n < 0)
 	{
-		count += _putchar('-');
+		count += putchar('-');
 		n = -n;
 	}
 
 	if (n / 10)
 		count += print_integer(n / 10);
 
-	count += _putchar(n % 10 + '0');
+	count += putchar(n % 10 + '0');
 
 	return (count);
 }}
