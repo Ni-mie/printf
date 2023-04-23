efdf9c6479855fddbc808ef8bcf5158687f64407
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-04-23 13:49:38 +0100

    another method

--- _printf.c
+++ _printf.c
@@ -6,45 +6,35 @@
  */
 int _printf(const char * const format, ...)
 {
-	int len = 0;
-	const char *p = format;
+	convert t[] = {
+		{"%s", print_string}, {"%c", print_char},
+		{"%%", print_percent},
+		{"%i", print_int}, {"%d", print_dec}
+	};
 
 	va_list args;
+	int i = 0, j, len = 0;
 
 	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
 
-	while (*p != '\0')
+	while (format[i] != '\0')
 	{
-		if (*p == '%')
+		j = 5;
+		while (j >= 0)
 		{
-			p++;
-			switch (*p)
+			if (t[j].id[0] == format[i]
+			    && t[j].id[1] == format[i + 1])
 			{
-			case 's':
-				len += print_string(args);
-				break;
-			case 'c':
-				len += print_char(args);
-				break;
-			case '%':
-				len += print_percent();
-				break;
-			case 'i': case 'd':
-				len += print_int(args);
-				break;
-			default:
-				putchar('%');
-				putchar(*p);
-				len += 2;
-				break;
+				len += t[j].f(args);
+				i = i + 2;
 			}
-			p++;
+			j--;
 		}
-		else
-		{
-			putchar(*p++);
-			len++;
-		}
+		putchar(format[i]);
+		len++;
+		i++;
 	}
 	va_end(args);
 	return (len);
