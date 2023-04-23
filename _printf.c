fbda69c36088b8543914c6ad53ea7773420a0dfc
Ebunoluwa Owoeye <owoeyeebun46@gmail.com>
2023-04-23 13:16:31 +0100

    fixing

--- _printf.c
+++ _printf.c
@@ -6,32 +6,45 @@
  */
 int _printf(const char * const format, ...)
 {
-	convert t[] = {
-		{"%s", print_string}, {"%c", print_char},
-		{"%%", print_percent}, {"%i", print_int}, {"%d", print_dec}
-	};
+	int len = 0;
+	const char *p = format;
+
 	va_list args;
-	int i = 0, j, len = 0;
 
 	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
 
-	while (format[i] != '\0')
+	while (*p != '\0')
 	{
-		while (j >= 0)
+		if (*p == '%')
 		{
-			if (t[j].id[0] == format[i]
-			    && t[j].id[1] == format[i + 1])
+			p++;
+			switch (*p)
 			{
-				len += t[j].f(args);
-				i = i + 2;
+			case 's':
+				len += print_string(args);
+				break;
+			case 'c':
+				len += print_char(args);
+				break;
+			case '%':
+				len += print_percent();
+				break;
+			case 'i': case 'd':
+				len += print_int(args);
+				break;
+			default:
+				putchar('%');
+				putchar(*p);
+				len += 2;
+				break;
 			}
-			j--;
+			p++;
 		}
-		putchar(format[i]);
-		len++;
-		i++;
+		else
+		{
+			putchar(*p++);
+			len++;
+		}
 	}
 	va_end(args);
 	return (len);
