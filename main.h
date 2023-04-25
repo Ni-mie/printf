f8d6a0440de47d2f6baaca7b326e2503a2036127
izebhigie <dizebhigie@gmail.com>
2023-04-25 22:24:51 +0100

    width handler

--- main.h
+++ main.h
@@ -1,11 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
 
 #include <stdio.h>
 #include <stdarg.h>
@@ -45,6 +52,21 @@ int print_string(va_list args, ...);
 int print_char(va_list args, ...);
 int _putchar(char c);
 int _printf(const char *format, ...);
+
+
+int get_width(const char *format, int *i, va_list);
+/* width handler */
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+	int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
+int write_unsgnd(int is_negative, int ind,
+char buffer[],
+	int flags, int width, int precision, int size);
 
 
 #endif
